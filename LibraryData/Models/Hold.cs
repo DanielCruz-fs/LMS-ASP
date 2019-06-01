795f1241919dbe1b452d91ded4850ff3fa69a1ec
unknown <dannyfsociety@gmail.com>
2019-06-01 11:50:30 -0400

    building core classes

---
+++ LibraryData/Models/Hold.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class Hold
+    {
+        public int Id { get; set; }
+
+        public DateTime HoldPlace { get; set; }
+
+        //fkeys
+        public virtual LibraryAsset LibraryAsset { get; set; }
+
+        public virtual LibraryCard LibraryCard { get; set; }
+    }
+}
