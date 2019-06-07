ba2e82236ee41dfb55e0135ccd0e92057964ae2e
unknown <dannyfsociety@gmail.com>
2019-06-07 12:03:22 -0400

    Patron back implementation service and controller:rocket:

---
+++ Library/Controllers/PatronController.cs
@@ -0,0 +1,39 @@
+﻿using Library.Models.Patron;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class PatronController : Controller
+    {
+        private readonly IPatron patron;
+
+        public PatronController(IPatron patron)
+        {
+            this.patron = patron;
+        }
+
+        public IActionResult Index()
+        {
+            var allPatrons = this.patron.GetAll();
+
+            var patronModels = allPatrons.Select(p => new PatronDetailModel
+            {
+                Id = p.Id,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                LibraryCardId = p.LibraryCard.Id,
+                OverdueFees = p.LibraryCard.Fees,
+                HomeLibraryBranch = p.LibraryBranch.Name
+            }).ToList();
+
+            var model = new PatronIndexModel { Patrons = patronModels };
+
+            return View(model);
+        }
+    }
+}
--- Library/Startup.cs
+++ Library/Startup.cs
@@ -43,6 +43,7 @@ namespace Library
             services.AddSingleton(Configuration);
             services.AddScoped<ILibraryAsset, LibraryAssetService>();
             services.AddScoped<ICheckout, CheckoutService>();
+            services.AddScoped<IPatron, PatronService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
